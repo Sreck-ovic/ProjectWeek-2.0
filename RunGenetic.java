d5332eda4f6cda556c73bd8b0647395f56d5b72e
readjfb <readjfb@aol.com>
2019-11-14 13:39:02 -0600

    added progress bar

--- RunGenetic.java
+++ RunGenetic.java
@@ -1,26 +1,25 @@
 import java.util.ArrayList;
 
 public class RunGenetic{
-
-
-
 	public static void main(String args[]){
 		int totalGenerations = 10000;
 		System.out.println("Setting Up Algorithm #1");
 		ArrayList<Integer> test = new ArrayList<Integer>();
-		//test.add(149);
-		//test.add(167);
+		
 		GeneticAlgo gen = new GeneticAlgo("jdbc:sqlite:pweek.db", new ArrayList<Integer>());
 		gen.populate(100);
 		gen.sort();
 //		gen.getTopReg().outputResults();
 
 		System.out.println("Genetic Algorithm #1");
+
 		for(int i=0;i<totalGenerations;i++){
-			//System.out.println("Generation "+i);
+			System.out.print("\rGeneration "+ i + "/"+totalGenerations);
 			gen.killandmate();
 			gen.sort();
 		}
+		System.out.println("");
+
 		ArrayList<Integer> bad = gen.getTopReg().outputResults();
 		System.out.println("Bad = " + bad);
 		System.out.println("Setting Up Algorithm #2");
@@ -34,18 +33,11 @@ public class RunGenetic{
 
 		System.out.println("Genetic Algorithm #2");
 		for(int i=0;i<totalGenerations;i++){
-		//	System.out.println("Generation "+i);
+			System.out.print("\rGeneration "+i + "/"+totalGenerations);
 			gen2.killandmate();
 			gen2.sort();
 		}
+		System.out.println("");
 		gen2.getTopReg().outputResultstoCSV();
-
-
-
-
-
-
-
-
 	}
 }
